0b78aec1036585c62caed47bf1fc646f9cf94841
phobos <phobos@RB16>
2020-08-23 06:13:34 +0200

    STM32 nucleo projekt

---
+++ nucleoA0/Debug/Src/stm32f1xx_hal_msp.d
@@ -0,0 +1,73 @@
+Src/stm32f1xx_hal_msp.o: ../Src/stm32f1xx_hal_msp.c \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Inc/main.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Inc/stm32f1xx_hal_conf.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/core_cm3.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Inc/main.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Inc/stm32f1xx_hal_conf.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/core_cm3.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_adc_ex.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+/home/orion/STprojects/wide_led_pa1/nucleoA0/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
